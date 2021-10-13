fedf2b0e5f183129dac6a9201e8530ce717abf3f
Jeremy Rotter <jeremy.rotter@gmail.com>
2021-10-12 22:22:30 -0400

    First chunk of FEN parsing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod reset; //include "reset/mod.rs"
 
 fn main() {
       let r = reset::new();
+      let starting_fen = String::from("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+      let s = reset::from_fen(starting_fen);
       println!("Hello world!");
       reset::sub1::hello();
       r.print();

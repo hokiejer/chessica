9091c3806ad14ef42efac859fdc27c4894440f68
Jeremy Rotter <jrotter@trhc.com>
2022-07-16 17:53:44 +0000

    Generate Next Move: first steps

--- src/reset/moves.rs
+++ src/reset/moves.rs
@@ -1,5 +1,5 @@
 use crate::reset::Reset;
-use crate::utils;
+use crate::bitops;
 
 impl Reset {
 
@@ -11,7 +11,6 @@ impl Reset {
     /// let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     /// r.init_from_fen(fen.to_string());
     /// r.initialize_move_generation();
-    /// # assert_eq!(r.move_id,10);
     /// ```
     pub fn initialize_move_generation(&mut self) {
         if self.to_move == 0 {
@@ -19,45 +18,41 @@ impl Reset {
         } else {
             self.b_current_piece = bitops::lowest_bit(self.b_black);
         }
-        self.current_piece = utils:convert_bitstring_to_square(self.b_current_piece);
+        self.current_piece = bitops::get_bit_number(self.b_current_piece);
         self.move_id = 10;	//Prime the first move
     }
 
-}
 
-#[cfg(test)]
-mod tests {
-    use crate::reset;
-    #[test]
-    fn move_init_move_generation() {
-        let mut r = chessica::reset::new();
-        let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-        r.init_from_fen(fen.to_string());
-        r.initialize_move_generation();
-        assert_eq!(r.b_current_piece,0x0000000000000001,"b_current_piece");
-        assert_eq!(r.current_piece,1,"current_piece");
-        assert_eq!(r.move_id,10,"move_id");
-
-        let mut r = chessica::reset::new();
-        let fen2 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1";
-        r.init_from_fen(fen2.to_string());
-        r.initialize_move_generation();
-        assert_eq!(r.b_current_piece,0x0001000000000000,"b_current_piece");
-        assert_eq!(r.current_piece,49,"current_piece");
-        assert_eq!(r.move_id,10,"move_id");
+    /// Generate the next move for a Reset
+    ///
+    /// Expects the child reset to already be initialized from
+    /// the parent
+    ///
+    /// # Examples
+    /// ```
+    /// let mut r = chessica::reset::new();
+    /// let mut child = chessica::reset::new();
+    /// let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    /// r.init_from_fen(fen.to_string());
+    /// r.initialize_move_generation();
+    /// r.generate_next_move(&mut child);
+    /// # assert_eq!(0,1);
+    /// ```
+    pub fn generate_next_move(&mut self, child: &mut Reset) {
+        if self.to_move == 0 {  // White's Move
+            while self.b_current_piece != 0 {
+                // do stuff
+                self.b_current_piece = bitops::next_lowest_bit(self.b_white, self.b_current_piece); // Someday, I can create a Reset method for this called look_for_next_movable_piece
+            }
+        } else {    //Black's Move
+            while self.b_current_piece != 0 {
+                // do stuff
+                self.b_current_piece = bitops::next_lowest_bit(self.b_black, self.b_current_piece); // Someday, I can create a Reset method for this called look_for_next_movable_piece
+            }
+        }
+        todo!();
     }
 }
-
-
-
-/// Initialize move generation for a Reset
-//void Reset::InitializeMoveGeneration()
-//{
-//}
-
-
-/// Generate the next move for a Reset
-
 ////
 ////PERFORMANCE PATH
 ////
@@ -184,4 +179,27 @@ mod tests {
 //}
 //
 
+
+#[cfg(test)]
+mod tests {
+    use crate::reset;
+    #[test]
+    fn move_init_move_generation() {
+        let mut r = reset::new();
+        let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+        r.init_from_fen(fen.to_string());
+        r.initialize_move_generation();
+        assert_eq!(r.b_current_piece,0x0000000000000001,"b_current_piece");
+        assert_eq!(r.current_piece,1,"current_piece");
+        assert_eq!(r.move_id,10,"move_id");
+
+        let mut r = reset::new();
+        let fen2 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1";
+        r.init_from_fen(fen2.to_string());
+        r.initialize_move_generation();
+        assert_eq!(r.b_current_piece,0x0001000000000000,"b_current_piece");
+        assert_eq!(r.current_piece,49,"current_piece");
+        assert_eq!(r.move_id,10,"move_id");
+    }
+}
 

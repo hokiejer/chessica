9060c60c2e77e71bd242f375c71a5094ff6550cb
Jeremy Rotter <jrotter@trhc.com>
2022-08-08 00:52:05 +0000

    White pawn captures

--- src/reset/pawn.rs
+++ src/reset/pawn.rs
@@ -1,11 +1,16 @@
 use crate::reset::Reset;
 use crate::reset::r#const::B_NOT_TOP_EDGE;
+use crate::reset::r#const::B_NOT_UL_EDGE;
+use crate::reset::r#const::B_NOT_UR_EDGE;
+use crate::reset::r#const::B_NOT_DL_EDGE;
+use crate::reset::r#const::B_NOT_DR_EDGE;
 use crate::reset::r#const::B_STARTING_PAWN_POSITION;
 
 impl Reset {
     pub fn generate_next_white_pawn_move(&mut self, child: &mut Reset) -> bool {
         // Forward one
         if self.move_id == 10 {
+            println!("Considering forward one");
             self.move_id = 20;
             let b_destination = self.b_current_piece << 8;
             if (b_destination & B_NOT_TOP_EDGE != 0) && 
@@ -18,6 +23,7 @@ impl Reset {
                 
         // Forward two
         if self.move_id == 20 {
+            println!("Considering forward two");
             self.move_id = 30;
             let b_one_square = self.b_current_piece << 8;
             let b_destination = self.b_current_piece << 16; // This won't work as coded
@@ -34,12 +40,31 @@ impl Reset {
 
         // Capture Left
         if self.move_id == 30 {
+            println!("Considering capture left");
             self.move_id = 40;
+            let b_destination = self.b_current_piece << 9;
+            if (self.b_current_piece & B_NOT_UL_EDGE != 0) && 
+                (b_destination & self.b_black != 0) && 
+                self.add_move_if_valid(child, b_destination) 
+            {
+                return true;
+            }
         }
 
         // Capture Right
-        if self.move_id == 30 {
-            self.move_id = 40;
+        if self.move_id == 40 {
+            println!("Considering capture right");
+            self.move_id = 50;
+            let b_destination = self.b_current_piece << 7;
+            println!("b_destination == {:x}",b_destination);
+            println!("B_NOT_UR_EDGE == {:x}",B_NOT_UR_EDGE);
+            println!("self.b_black == {:x}",self.b_black);
+            if (self.b_current_piece & B_NOT_UR_EDGE != 0) && 
+                (b_destination & self.b_black != 0) && 
+                self.add_move_if_valid(child, b_destination) 
+            {
+                return true;
+            }
         }
 
         self.consider_next_moveable_piece();
@@ -256,5 +281,93 @@ mod tests {
         assert_eq!(r.b_current_piece,0);
         assert_eq!(r.move_id,10);
 
+    }
+
+    #[test]
+    fn pawn_moves_white_captures() {
+        let mut r = prep_board("k7/8/2p2p2/3PP3/p4p1p/1P4P1/P1P5/K7 w - - 0 1");
+        let mut child = reset::new();
+        r.b_current_piece = utils::convert_square_to_bitstring("g3".to_string());
+
+        // g3 to g4
+        let fen = String::from("k7/8/2p2p2/3PP3/p4pPp/1P6/P1P5/K7 b - - 0 1");
+        let retval = r.generate_next_pawn_move(&mut child);
+        assert!(retval);
+        assert_eq!(child.to_fen(),fen);
+        assert_eq!(r.b_current_piece,utils::convert_square_to_bitstring("g3".to_string()));
+        assert_eq!(r.move_id,20);
+        assert_eq!(child.capture,0);
+
+        // g3 to f4
+        let fen = String::from("k7/8/2p2p2/3PP3/p4P1p/1P6/P1P5/K7 b - - 0 1");
+        let retval = r.generate_next_pawn_move(&mut child);
+        assert!(retval);
+        assert_eq!(child.to_fen(),fen);
+        assert_eq!(r.b_current_piece,utils::convert_square_to_bitstring("g3".to_string()));
+        assert_eq!(r.move_id,40);
+        assert_eq!(child.capture,1);
+
+        // g3 to h4
+        let fen = String::from("k7/8/2p2p2/3PP3/p4p1P/1P6/P1P5/K7 b - - 0 1");
+        let retval = r.generate_next_pawn_move(&mut child);
+        assert!(retval);
+        assert_eq!(child.to_fen(),fen);
+        assert_eq!(r.b_current_piece,utils::convert_square_to_bitstring("g3".to_string()));
+        assert_eq!(r.move_id,50);
+        assert_eq!(child.capture,1);
+
+        // No more moves for this pawn
+        let retval = r.generate_next_pawn_move(&mut child);
+        assert!(!retval);
+        assert_eq!(r.b_current_piece,utils::convert_square_to_bitstring("b3".to_string()));
+        assert_eq!(r.move_id,10);
+
+        // b3 to b4
+        let fen = String::from("k7/8/2p2p2/3PP3/pP3p1p/6P1/P1P5/K7 b - - 0 1");
+        let retval = r.generate_next_pawn_move(&mut child);
+        assert!(retval);
+        assert_eq!(child.to_fen(),fen);
+        assert_eq!(r.b_current_piece,utils::convert_square_to_bitstring("b3".to_string()));
+        assert_eq!(r.move_id,20);
+        assert_eq!(child.capture,0);
+
+        // b3 to a4
+        let fen = String::from("k7/8/2p2p2/3PP3/P4p1p/6P1/P1P5/K7 b - - 0 1");
+        let retval = r.generate_next_pawn_move(&mut child);
+        assert!(retval);
+        assert_eq!(child.to_fen(),fen);
+        assert_eq!(r.b_current_piece,utils::convert_square_to_bitstring("b3".to_string()));
+        assert_eq!(r.move_id,40);
+        assert_eq!(child.capture,1);
+
+        // No more moves for this pawn
+        let retval = r.generate_next_pawn_move(&mut child);
+        assert!(!retval);
+        assert_eq!(r.b_current_piece,utils::convert_square_to_bitstring("e5".to_string()));
+        assert_eq!(r.move_id,10);
+
+        // e5 to e6
+        let fen = String::from("k7/8/2p1Pp2/3P4/p4p1p/1P4P1/P1P5/K7 b - - 0 1");
+        let retval = r.generate_next_pawn_move(&mut child);
+        assert!(retval);
+        assert_eq!(child.to_fen(),fen);
+        assert_eq!(r.b_current_piece,utils::convert_square_to_bitstring("e5".to_string()));
+        assert_eq!(r.move_id,20);
+        assert_eq!(child.capture,0);
+
+        // e5 to f5
+        let fen = String::from("k7/8/2p2P2/3P4/p4p1p/1P4P1/P1P5/K7 b - - 0 1");
+        let retval = r.generate_next_pawn_move(&mut child);
+        assert!(retval);
+        assert_eq!(child.to_fen(),fen);
+        assert_eq!(r.b_current_piece,utils::convert_square_to_bitstring("e5".to_string()));
+        assert_eq!(r.move_id,50);
+        assert_eq!(child.capture,1);
+
+        // No more moves for this pawn
+        let retval = r.generate_next_pawn_move(&mut child);
+        assert!(!retval);
+        assert_eq!(r.b_current_piece,utils::convert_square_to_bitstring("d5".to_string()));
+        assert_eq!(r.move_id,10);
     }
 }

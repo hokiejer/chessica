f9d4bdc6a745437708c9d93f7611eb20c13f0a51
hokiejer <jeremy.rotter@gmail.com>
2023-01-30 03:37:51 +0000

    Operator message sending cleanup

--- src/operator/instructions.rs
+++ src/operator/instructions.rs
@@ -1,28 +1,26 @@
 use crate::operator::Operator;
-use crate::operator::OperatorMessage;
-use crate::operator::message::OperatorInstruction::ExitProgram;
-use crate::operator::message::OperatorInstruction::TestInstruction;
+use crate::operator::message;
 
 impl Operator {
 
     // Process an instruction, communicating with the Orchestrator as appropriate.
     pub fn process_instruction(&mut self, instruction: &String) {
         println!("Input == {}",instruction);
-        if instruction == "quit" {
-            let mut message = OperatorMessage {
-                instruction: ExitProgram,
-                data_string: "".to_string(),
-            };
-            self.orchestrator_transmit_channel.as_ref().unwrap().send(message).unwrap();
-        } else if instruction == "protover 2" {
-            println!("feature done=1")
-        } else {
-            let mut message = OperatorMessage {
-                instruction: TestInstruction,
-                data_string: "".to_string(),
-            };
+        let mut message = message::new();
+        match instruction.as_str() {
+            "quit" => {
+                message.exit_program();
+            },
+            "force" => {
+                message.player_status_change(false, false);
+            },
+            _ => {
+
+            },
+        }
+        if message.sendable() {
             self.orchestrator_transmit_channel.as_ref().unwrap().send(message).unwrap();
         }
-    }
+     }
 
 }
--- src/operator/message.rs
+++ src/operator/message.rs
@@ -1,21 +1,54 @@
 #[derive(PartialEq,Eq,Copy,Clone,Hash,Debug)]
 pub enum OperatorInstruction {
-    TestInstruction,
+    PlaceHolder,
     NewBoard,
     MoveTaken,
     PlayerStatusChange,
     ExitProgram,
 }
 
+use crate::operator::message::OperatorInstruction::PlaceHolder;
+use crate::operator::message::OperatorInstruction::PlayerStatusChange;
+use crate::operator::message::OperatorInstruction::ExitProgram;
+
 #[derive(PartialEq,Eq,Clone,Hash,Debug)]
 pub struct OperatorMessage {
     pub instruction: OperatorInstruction,
     pub data_string: String,
+    pub computer_white: bool,
+    pub computer_black: bool,
 }
 
-pub fn new(instruction: OperatorInstruction, datastring: String) -> OperatorMessage {
+pub fn new() -> OperatorMessage {
     OperatorMessage {
-        instruction: instruction,
-        data_string: datastring,
+        instruction: PlaceHolder,
+        data_string: "".to_string(),
+        computer_white: false,
+        computer_black: false,
     }
 }
+
+impl OperatorMessage {
+    pub fn new_board(&mut self) {
+        self.instruction = ExitProgram;
+    }
+
+    pub fn move_taken(&mut self) {
+        self.instruction = ExitProgram;
+    }
+
+    pub fn player_status_change(&mut self, computer_white: bool, computer_black: bool) {
+        self.instruction = PlayerStatusChange;
+        self.computer_white = computer_white;
+        self.computer_black = computer_black;
+    }
+
+    pub fn exit_program(&mut self) {
+        self.instruction = ExitProgram;
+    }
+
+    pub fn sendable(&mut self) -> bool {
+        self.instruction != PlaceHolder
+    }
+}
+

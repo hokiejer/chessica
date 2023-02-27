1addaf3e8e97baf807288202ca4c6ac26d0058de
hokiejer <jeremy.rotter@gmail.com>
2023-02-27 02:42:55 +0000

    Pre-sort after search

--- src/cogitator/mod.rs
+++ src/cogitator/mod.rs
@@ -62,19 +62,25 @@ impl Cogitator {
     }
 
     /// Run Chessica's Cogitator
-    pub fn run(&self) {
-        self.search();
+    pub fn run(&mut self) {
+        self.search(6);
+        self.barrier.wait();
+        println!("Mind the gap!");
+        self.barrier.wait();
+        self.pre_sort_children();
+        self.barrier.wait();
+
     }
 
-    pub fn search(&self) {
+    pub fn search(&mut self, depth: u8) {
 
         let mut locked_trees = Vec::new();
-        for tree in &(self.children) {
+        for tree in &mut self.children {
             if let Ok(mut tree) = tree.try_lock() {
                 let mut move_count: u64 = 0;
                 let (success, score) = tree.alpha_beta_promote_prune_parallel(
                     0,
-                    6,
+                    depth,
                     &(self.global_min),
                     &(self.global_max),
                     &(self.red_light),
@@ -99,21 +105,39 @@ impl Cogitator {
             }
         }
         self.barrier.wait();
-        //This will clear the locks
-        locked_trees.clear();
-        println!("Mind the gap!");
-        self.barrier.wait();
-        println!("Leader has to sort here!");
-        self.barrier.wait();
-
     }
 
+    pub fn pre_sort_children(&mut self) {
+        //let mut locked_trees = Vec::new();
+        if self.id == 0 {
+            println!("Leader has to sort here!");
+            let mut i = 0;
+            let mut j = self.children.len() - 1;
+            while i <= j {
+                if self.children[i].lock().unwrap().score != None {
+                    i += 1;
+                    continue;
+                }
+                if self.children[j].lock().unwrap().score == None {
+                    j -= 1;
+                    continue;
+                }
+                self.children.swap(i,j);
+                i += 1;
+                j -= 1;
+            }
+        }
+    }
 
 }
 
 #[cfg(test)]
 mod tests {
     use crate::cogitator;
+    use std::sync::{Arc,Mutex,Barrier};
+    use crate::tree;
+    use tree::Tree;
+    use std::sync::atomic::{AtomicBool,AtomicI32};
 
     #[test]
     fn new_cogitator() {
@@ -121,4 +145,34 @@ mod tests {
         //assert_eq!(o.x,0);
     }
 
+    #[test]
+    fn sorting() {
+        let barrier = Arc::new(Barrier::new(1));
+        let min = Arc::new(AtomicI32::new(1));
+        let max = Arc::new(AtomicI32::new(2));
+        let red_light = Arc::new(AtomicBool::new(false));
+        let exit_signal = Arc::new(AtomicBool::new(false));
+        let mut c = cogitator::new(0, barrier, min, max, true, red_light, exit_signal);
+
+        let mut tree_list: Vec<Arc<Mutex<Tree>>> = Vec::new();
+        let scores = vec![None, Some(123), None, Some(-123), Some(12), Some(100), None];
+        for score in scores {
+            let mut t = tree::new();
+            t.score = score;
+            tree_list.push(Arc::new(Mutex::new(t)));
+        }
+        c.set_child_list(tree_list.clone());
+        c.pre_sort_children();
+        for tree in c.children {
+            match tree.lock().unwrap().score {
+                Some(this_score) => {
+                    println!("Score == {}",this_score);
+                },
+                None => {
+                    println!("Found a node with no score");
+                },
+            }
+        }
+        assert!(false);
+    }
 }
